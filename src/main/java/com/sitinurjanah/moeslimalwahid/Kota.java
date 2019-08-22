ab2ee2a437df286e888e0a7af61400d93e1dc46b
sitinurjanah08 <nurjanahchity08@gmail.com>
2019-08-22 21:10:40 +0700

    Initial commit

---
+++ src/main/java/com/sitinurjanah/moeslimalwahid/Kota.java
@@ -0,0 +1,27 @@
+package com.sitinurjanah.moeslimalwahid;
+
+public class Kota {
+    private int id;
+    private String nama;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String toString(){
+        return  nama.toString();
+    }
+}
+
